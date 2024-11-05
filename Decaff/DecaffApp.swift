a534748c203a3b19517b2a3f5244880b377e616a
Irakli Grigolia <igrigolia@wpi.edu>
2024-11-05 08:19:47 -0500

    Initial Commit

---
+++ Decaff/DecaffApp.swift
@@ -0,0 +1,32 @@
+//
+//  DecaffApp.swift
+//  Decaff
+//
+//  Created by Tazi Grigolia on 11/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DecaffApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DecaffTests/DecaffTests.swift
@@ -0,0 +1,17 @@
+//
+//  DecaffTests.swift
+//  DecaffTests
+//
+//  Created by Tazi Grigolia on 11/5/24.
+//
+
+import Testing
+@testable import Decaff
+
+struct DecaffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
